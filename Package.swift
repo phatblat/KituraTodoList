146609ed1f471391589ee9f848a32657cbd4f22f
Ben Chatelain <ben@octop.ad>
2017-06-11 17:55:36 -0600

    Configure SwiftPM to use Swift 4
    Xcode project generated to use Swift 3.2?

--- Package.swift
+++ Package.swift
@@ -19,5 +19,6 @@ let package = Package(
         .testTarget(
             name: "KituraTodoListTests",
             dependencies: ["KituraTodoList"]),
-    ]
+    ],
+    swiftLanguageVersions: [4]
 )
