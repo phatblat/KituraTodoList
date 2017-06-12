17c188a667261063c2e6685855f3cccf83d9bd8e
Ben Chatelain <ben@octop.ad>
2017-06-12 14:05:00 -0600

    Get example test running

--- Sources/KituraTodoList/Models/TodoItem.swift
+++ Sources/KituraTodoList/Models/TodoItem.swift
@@ -9,11 +9,20 @@
 import Foundation
 
 struct TodoItem {
-    var id: String = ""
-    var order: Int = 0
-    var title: String = ""
-    var completed: Bool = false
-    var url: String = ""
+    var id: String
+    var order: Int
+    var title: String
+    var completed: Bool
+    var url: String
+
+    init(id: String = "", order: Int = 0, title: String = "", completed: Bool = false,
+                url: String = "") {
+        self.id = id
+        self.order = order
+        self.title = title
+        self.completed = completed
+        self.url = url
+    }
 
     func serialize() -> JSONDictionary {
         var result = JSONDictionary()
--- Tests/KituraTodoListTests/KituraTodoListTests.swift
+++ Tests/KituraTodoListTests/KituraTodoListTests.swift
@@ -1,14 +1,11 @@
+//@testable
+import KituraTodoList
 import XCTest
-@testable import KituraTodoList
 
 class KituraTodoListTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(KituraTodoList().text, "Hello, World!")
+        XCTAssertEqual(TodoItem().id, "")
     }
-
 
     static var allTests: [(String, (KituraTodoListTests) -> () -> Void)] = [
         ("testExample", testExample),
