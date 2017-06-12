79ce345f8f912bf5ece2abfadac154514ed7ac96
Ben Chatelain <ben@octop.ad>
2017-06-11 23:33:13 -0600

    Update RouterMiddleware API in AllRemoteOriginMiddleware

--- Sources/KituraTodoList/Controllers/App.swift
+++ Sources/KituraTodoList/Controllers/App.swift
@@ -6,15 +6,14 @@
 //
 //
 
+import Kitura
 import Foundation
 
 class AllRemoteOriginMiddleware: RouterMiddleware {
-    func handle(request: RouterRequest, response: RouterResponse, next: () -> Void) {
-        response.setHeader("Access-Control-Allow-Origin", value: "*")
-        response.setHeader("Access-Control-Allow-Headers", value: "accept, content-type")
-        response.setHeader("Access-Control-Allow-Methods", value: "GET,HEAD,POST,DELETE,OPTIONS,PUT")
+    func handle(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "accept, content-type"
+        response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,POST,DELETE,OPTIONS,PUT"
         next()
     }
 }
-
-router.use("/*", middleware: AllRemoteOriginMiddleware())
